c39c3a7cf1e084a45aa2663eedea1a0872e0f0b0
Suhanii-13 <suhanishaikh.1304@gmail.com>
2024-10-31 19:31:14 +0530

    update

--- routes/booking.js
+++ routes/booking.js
@@ -5,21 +5,20 @@ const { isLoggedIn } = require("../middleware");
 const wrapAsync = require("../utils/wrapAsync") ;
 const cron = require("node-cron");
 
-
-// This will run every 5 seconds for testing
-cron.schedule("*/30 * * * * *", async () => {
-  // Runs every 5 seconds
+// This will delete bookings once approved or rejected by admin, 7 days after creation, 
+//from both admin and user side
+cron.schedule("0 0 * * *", async () => {
   try {
     const now = new Date();
-    const time5SecAgo = new Date(now.getTime() - 5 * 1000); 
+    const time7DaysAgo = new Date(now.getTime() - 7 * 24 * 60 * 60 * 1000); 
 
     const deletedBookings = await Booking.deleteMany({
-      createdAt: { $lt: time5SecAgo }, 
-      status: { $in: ["approved", "rejected"] }, 
+      createdAt: { $lt: time7DaysAgo }, 
+      status: { $in: ["approved", "rejected"] },
     });
 
     console.log(
-      `Auto-deleted ${deletedBookings.deletedCount} bookings older than 5 seconds`
+      `Auto-deleted ${deletedBookings.deletedCount} bookings older than 7 days`
     );
   } catch (error) {
     console.error("Error during auto-delete of bookings:", error);
