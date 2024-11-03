92dda21bcda9733acb0a9bf646e02d64a55ff695
Suhanii-13 <suhanishaikh.1304@gmail.com>
2024-11-03 12:35:48 +0530

    update booking route

--- routes/booking.js
+++ routes/booking.js
@@ -3,18 +3,27 @@ const router = express.Router();
 const userBooking = require("../controllers/userBooking");
 const adminBooking = require("../controllers/adminBooking");
 const bookingController = require("../controllers/booking");
-
 const { isLoggedIn } = require("../middleware");
 
+//booking will auto delete after 7 days ones it will aprove or rejected by admin
 bookingController.scheduleAutoDelete();
+
+//home route
 router.get("/home", bookingController.renderHome);
 
 // User-specific routes
-router.get("/new", isLoggedIn, userBooking.renderNewBookingForm);
-router.post("/new", isLoggedIn, userBooking.createBooking);
+router
+  .route("/new")
+  .get(isLoggedIn, userBooking.renderNewBookingForm)
+  .post(isLoggedIn, userBooking.createBooking);
+
 router.get("/msg", isLoggedIn, userBooking.viewUserBookings);
-router.get("/msg/edit/:id", isLoggedIn, userBooking.renderEditBookingForm);
-router.patch("/msg/edit/:id", isLoggedIn, userBooking.updateBooking);
+
+router
+  .route("/msg/edit/:id")
+  .get(isLoggedIn, userBooking.renderEditBookingForm)
+  .patch(isLoggedIn, userBooking.updateBooking);
+
 router.delete("/msg/delete/:id", isLoggedIn, userBooking.deleteBooking);
 
 // Admin-specific routes
